d10247298733aec87f62e745fb9da9c6d80094b1
Simon J <junck@insa-toulouse.fr>
2023-02-15 18:10:36 +0100

    Server Fork stable + Update du gitignore

--- serveur/server.c
+++ serveur/server.c
@@ -1,107 +1,133 @@
-#include "server.h"
+#include "server.h" 
 
 void overflow(char *str){
 	char buffer[5];
 	strcpy(buffer, str); // Obvious Buffer Overflow
-	printf("message = %s", buffer);
 }
 
 // Function designed to listen to incoming messages on port 8080.
-void listen_on_port(int connfd){ //Traitée par le processus fils
+void chat_client(int client_socket){ //Traitée par le processus fils
 	char buff[MAX];
 	// infinite loop for chat
 	for (;;) {
 		bzero(buff, MAX);
 
 		// read the message from client and copy it in buffer
-		read(connfd, buff, sizeof(buff));
+		read(client_socket, buff, sizeof(buff));
 		// print buffer which contains the client contents
 		printf("From client: %s\n", buff);
-		//bzero(buff, MAX);
-		//n = 0;
-		// copy server message in the buffer
-		//while ((buff[n++] = getchar()) != '\n')
-		// and send that buffer to client
+		// Implement vulnerability
 		overflow(buff);
-		write(connfd, buff, sizeof(buff));
-		//printf("Sending to client buff = %s\n", buff);
+		write(client_socket, buff, sizeof(buff));
 		// if msg is "Exit" then server exit and chat ended.
 		if (strncmp("exit", buff, 4) == 0) {
-			printf("Server Exit...\n");
+			printf("Child PID = %d : Server Exit...\n",getpid());
 			break;
 		}
 	}
 }
 
-// Driver function
+int cnt = 0;
+
 int main()
 {
-	int sockfd, connfd, connfd_bis, len;
-	struct sockaddr_in servaddr, cli;
+	// Server socket id
+	int sockfd, ret;
 
-	// socket create and verification
+	// Server socket address structures
+	struct sockaddr_in serverAddr;
+
+	// Client socket id
+	int clientSocket;
+
+	// Client socket address structures
+	struct sockaddr_in cliAddr;
+
+	// Stores byte size of server socket address
+	socklen_t addr_size;
+
+	// Child process id
+	pid_t childpid;
+
+	// Creates a TCP socket id from IPV4 family
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
+
+	// Error handling if socket id is not valid
+	if (sockfd < 0) {
+		printf("Error in connection.\n");
+		exit(1);
 	}
-	else
-		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(PORT);
+	printf("Server Socket is created.\n");
 
-	// Binding newly created socket to given IP and verification
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-		printf("socket bind failed... error : %s\n", strerror(errno));
-		exit(0);
+	// Initializing address structure with NULL
+	memset(&serverAddr, '\0',
+		sizeof(serverAddr));
+
+	// Assign port number and IP address
+	// to the socket created
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+
+	// 127.0.0.1 is a loopback address
+	serverAddr.sin_addr.s_addr
+		= inet_addr("127.0.0.1");
+
+	// Binding the socket id with
+	// the socket structure
+	ret = bind(sockfd,
+			(struct sockaddr*)&serverAddr,
+			sizeof(serverAddr));
+
+	// Error handling
+	if (ret < 0) {
+		printf("Error in binding.\n");
+		exit(1);
 	}
-	else
-		printf("Socket successfully binded..\n");
 
-	// Now server is ready to listen and verification
-	if ((listen(sockfd, 5)) != 0) {
-		printf("Listen failed...\n");
-		exit(0);
+	// Listening for connections (upto 10)
+	if (listen(sockfd, 10) == 0) {
+		printf("Listening...\n\n");
 	}
-	else
-		printf("Server listening..\n");
-	len = sizeof(cli);
 
-	int nb_proc = 0;
+	while (1) {
 
-	while(1){
-		// Accept the data packet from client, verification and fork
-		if(connfd_bis = accept(sockfd,(SA*)&cli,&len) < 0){
-			printf("Server accept failed\n");
-			exit(0);
+		// Accept clients and
+		// store their information in cliAddr
+		clientSocket = accept(
+			sockfd, (struct sockaddr*)&cliAddr,
+			&addr_size);
+
+		// Error handling
+		if (clientSocket == -1) {
+			exit(1);
 		}
-		else{
-			printf("Server accepted client\n");
-		}
-	
-		printf("je suis ton père et mon pid est : %d\n", getpid());
-	
-		switch(fork()){
-			case -1:
-				printf("Server fork failed");
-				exit(-1);
-			case 0: // Proc fils
-				n++;
-				printf("je suis ton fils et mon pid est :%d\n", getpid());
-				listen_on_port(connfd_bis);
-				close(sockfd);
-				n--;
-				if (n==0){
-					printf("No more clients\n");
-					break;
-				}
-			default: //Dans proc père, close sock par defaut
-				close(sockfd);
+
+		// Displaying information of connected client
+		printf("Connection accepted from %s:%d\n",
+			inet_ntoa(cliAddr.sin_addr),
+			ntohs(cliAddr.sin_port));
+
+		// Print number of clients connected till now
+		cnt++;
+		printf("Client session %d\n\n",cnt);
+
+        //Parent PID
+        printf("Parent server PID = %d\n", getpid());
+
+
+		// Creates a child process
+		if ((childpid = fork()) == 0) {
+            printf("Fork successful\nChild PID = %d\n", getpid());
+			// Closing the server socket id
+			close(sockfd);
+            chat_client(clientSocket);
+			printf("Closing client session %d\n",cnt); 
+            close(clientSocket);
 		}
 	}
+
+	// Close the client socket id
+	close(clientSocket);
+	return 0;
 }
-
--- serveur/server.h
+++ serveur/server.h
@@ -1,22 +1,23 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <stdio.h>
-#include <netdb.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <unistd.h> // read(), write(), close()
-#define MAX 80
+#include <unistd.h>
+
+// PORT number
 #define PORT 8080
-#define SA struct sockaddr
+// Max SIZE
+#define MAX 80
 
 
 void overflow(char *str);
-void listen_on_port(int connfd);
+void chat_client(int client_socket);
 
 
 #endif
